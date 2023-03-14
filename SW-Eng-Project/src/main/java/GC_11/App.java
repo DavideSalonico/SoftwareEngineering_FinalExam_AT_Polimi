34b8cf6bdc005f18438a563981531480deceeef8
MattiaRepetti <mattia.repetti@mail.polimi.it>
2023-03-14 15:54:22 +0100

    Fixed PersonalGoalCard

--- SW-Eng-Project/src/main/java/GC_11/App.java
+++ SW-Eng-Project/src/main/java/GC_11/App.java
@@ -4,9 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        PersonalGoalCard card = new PersonalGoalCard();
-        for (Triplet t : card.getGoalList()){
-            System.out.println("R " + t.getRow() + " C " + t.getCol() + " COLOR " + t.getColor());
-        }
+
     }
 }
--- SW-Eng-Project/src/main/java/GC_11/PersonalGoalCard.java
+++ SW-Eng-Project/src/main/java/GC_11/PersonalGoalCard.java
@@ -14,16 +14,13 @@ public class PersonalGoalCard {
     // Methods
 
     public PersonalGoalCard(){
-        Random RANDOM = new Random();
 
+        final int randomNumber = new Random().nextInt(13);
         goalList = new ArrayList<Triplet>();
 
-        for (int i=0; i<6;i++){
-            int randomRow = RANDOM.nextInt(6); // Generate a random row (0 to 5 rows)
-            int randomColumn = RANDOM.nextInt(5); // Generate a random column (0 to 4 columns)
-            TileColor randColor = TileColor.randomColor(); // Generate a random color
-            goalList.add(new Triplet (randomRow,randomColumn,randColor)); // Add to the list a random triplet with row = randY, column = i (0 to 5) and random color
-        }
+        // Read from a file one of the 12 personalGoalCards
+
+
     }
 
     public List<Triplet> getGoalList() {
