62dabc48994cc21a583a6940364b169f41c721b3
MattiaRepetti <mattia.repetti@mail.polimi.it>
2023-03-16 16:02:49 +0100

    Implemented constructor of class Board

--- SW-Eng-Project/src/main/java/GC_11/model/Board.java
+++ SW-Eng-Project/src/main/java/GC_11/model/Board.java
@@ -6,9 +6,95 @@ public class Board {
     private Bag bag;
 
 
-    public Board(int n){
+    public Board(int nPlayer){
+        chessBoard = new Tile[9][9];
+        for (int i =0; i<9;i++){
+            for (int j=0; j<9;j++){
+                chessBoard[i][j] = new Tile(TileColor.EMPTY);
+            }
+        }
+        initProhibitedCells();
+        if (nPlayer==2){
+            init2PlayersBoard();
+        }
+        else if(nPlayer==3){
+            init3PlayersBoard();
+        }
+
+
 
     }
+
+    private void initProhibitedCells(){
+        chessBoard[0][0] = new Tile(TileColor.PROHIBITED);
+        chessBoard[0][1] = new Tile(TileColor.PROHIBITED);
+        chessBoard[0][2] = new Tile(TileColor.PROHIBITED);
+        chessBoard[0][3] = new Tile(TileColor.PROHIBITED);
+        chessBoard[0][6]  = new Tile(TileColor.PROHIBITED);
+        chessBoard[0][7]  = new Tile(TileColor.PROHIBITED);
+        chessBoard[0][8] = new Tile(TileColor.PROHIBITED);
+        chessBoard[1][0] = new Tile(TileColor.PROHIBITED);
+        chessBoard[1][1] = new Tile(TileColor.PROHIBITED);
+        chessBoard[1][2] = new Tile(TileColor.PROHIBITED);
+        chessBoard[1][6] = new Tile(TileColor.PROHIBITED);
+        chessBoard[1][7] = new Tile(TileColor.PROHIBITED);
+        chessBoard[1][8] = new Tile(TileColor.PROHIBITED);
+        chessBoard[2][0]= new Tile(TileColor.PROHIBITED);
+        chessBoard[2][1]= new Tile(TileColor.PROHIBITED);
+        chessBoard[2][7]= new Tile(TileColor.PROHIBITED);
+        chessBoard[2][8] = new Tile(TileColor.PROHIBITED);
+        chessBoard[3][8] = new Tile(TileColor.PROHIBITED);
+        chessBoard[5][0]= new Tile(TileColor.PROHIBITED);
+        chessBoard[6][0] = new Tile(TileColor.PROHIBITED);
+        chessBoard[6][1] = new Tile(TileColor.PROHIBITED);
+        chessBoard[6][7] =  new Tile(TileColor.PROHIBITED);
+        chessBoard[6][8] = new Tile(TileColor.PROHIBITED);
+        chessBoard[7][0] = new Tile(TileColor.PROHIBITED);
+        chessBoard[7][1] = new Tile(TileColor.PROHIBITED);
+        chessBoard[7][2] = new Tile(TileColor.PROHIBITED);
+        chessBoard[7][6] = new Tile(TileColor.PROHIBITED);
+        chessBoard[7][8] = new Tile(TileColor.PROHIBITED);
+        chessBoard[8][0] = new Tile(TileColor.PROHIBITED);
+        chessBoard[8][1] = new Tile(TileColor.PROHIBITED);
+        chessBoard[8][2] = new Tile(TileColor.PROHIBITED);
+        chessBoard[8][5] = new Tile(TileColor.PROHIBITED);
+        chessBoard[8][6] = new Tile(TileColor.PROHIBITED);
+        chessBoard[8][7] = new Tile(TileColor.PROHIBITED);
+        chessBoard[8][8] = new Tile(TileColor.PROHIBITED);
+
+    }
+
+    private void init2PlayersBoard(){
+        chessBoard[0][4] = new Tile(TileColor.PROHIBITED);
+        chessBoard[0][5] = new Tile(TileColor.PROHIBITED);
+        chessBoard[1][3] = new Tile(TileColor.PROHIBITED);
+        chessBoard[2][2] = new Tile(TileColor.PROHIBITED);
+        chessBoard[2][6] = new Tile(TileColor.PROHIBITED);
+        chessBoard[3][0] = new Tile(TileColor.PROHIBITED);
+        chessBoard[3][7] = new Tile(TileColor.PROHIBITED);
+        chessBoard[4][0] = new Tile(TileColor.PROHIBITED);
+        chessBoard[4][8] = new Tile(TileColor.PROHIBITED);
+        chessBoard[5][1] = new Tile(TileColor.PROHIBITED);
+        chessBoard[5][8] = new Tile(TileColor.PROHIBITED);
+        chessBoard[6][2] = new Tile(TileColor.PROHIBITED);
+        chessBoard[6][6] = new Tile(TileColor.PROHIBITED);
+        chessBoard[7][5] = new Tile(TileColor.PROHIBITED);
+        chessBoard[8][3] = new Tile(TileColor.PROHIBITED);
+        chessBoard[8][4] = new Tile(TileColor.PROHIBITED);
+    }
+
+    private void init3PlayersBoard(){
+        chessBoard[0][4] = new Tile(TileColor.PROHIBITED);
+        chessBoard[1][3] = new Tile(TileColor.PROHIBITED);
+        chessBoard[3][7] = new Tile(TileColor.PROHIBITED);
+        chessBoard[4][0] = new Tile(TileColor.PROHIBITED);
+        chessBoard[4][8] = new Tile(TileColor.PROHIBITED);
+        chessBoard[5][1] = new Tile(TileColor.PROHIBITED);
+        chessBoard[7][5] = new Tile(TileColor.PROHIBITED);
+        chessBoard[8][4] = new Tile(TileColor.PROHIBITED);
+    }
+
+
     public Tile getTile(int x, int y){
         return chessBoard[x][y];
     }
