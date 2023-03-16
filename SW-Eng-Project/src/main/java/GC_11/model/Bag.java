f80cb7e07dedc88703e8c08b6f9a1e820c338863
MattiaRepetti <mattia.repetti@mail.polimi.it>
2023-03-16 15:03:48 +0100

    Implemented constructor of class Bag

--- SW-Eng-Project/src/main/java/GC_11/model/Bag.java
+++ SW-Eng-Project/src/main/java/GC_11/model/Bag.java
@@ -1,21 +1,29 @@
 package GC_11.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bag {
 
-
-
-    private List<Tile> tiles;
+    private List<Tile> tiles; // 132 tiles, 22 x 6
 
 
     public Bag(){
-
+        this.tiles = new ArrayList<Tile>();
+        for (int i=0; i< 22;i++){
+            tiles.add(new Tile(TileColor.WHITE));
+            tiles.add(new Tile(TileColor.BLUE));
+            tiles.add(new Tile(TileColor.GREEN));
+            tiles.add(new Tile(TileColor.CYAN));
+            tiles.add(new Tile(TileColor.PURPLE));
+            tiles.add(new Tile(TileColor.ORANGE));
+        }
     }
     public List<Tile> getTiles() {
         return tiles;
     }
 
+    // Used to insert tiles which was previously in the board before the refill
     public void insertTile(Tile t){
 
     }
