6aa7de89c472b0ab54510ade4da7d4106e7618f1
MattiaRepetti <mattia.repetti@mail.polimi.it>
2023-03-19 19:12:21 +0100

    Impemented BagTest class

--- SW-Eng-Project/src/test/java/GC_11/BagTest.java
+++ SW-Eng-Project/src/test/java/GC_11/BagTest.java
@@ -1,5 +1,44 @@
 package GC_11;
 
+import GC_11.model.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import static junit.framework.Assert.*;
+
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BagTest {
     //TODO: To be implemented by Mattia
+
+    @Test
+    public void rightNumberOfEachTile(){
+        Bag bag = new Bag();
+        ArrayList<Tile> tiles = (ArrayList<Tile>) bag.getTiles();
+        for (TileColor tc : TileColor.values()){
+            if(tc != TileColor.PROHIBITED && tc!= TileColor.EMPTY)
+                assertEquals(22,countTiles(tiles,tc));
+        }
+    }
+
+    @Test
+    public void rightNumberOfTiles(){
+        Bag bag = new Bag();
+        assertEquals(132, bag.getTiles().size());
+    }
+
+    private int countTiles(List<Tile> list,TileColor color)
+    {
+        int count = 0;
+        for(Tile t : list){
+            if(t.getColor() == color){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+
 }
