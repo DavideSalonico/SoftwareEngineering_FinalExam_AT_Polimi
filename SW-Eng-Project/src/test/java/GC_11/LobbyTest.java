ac49a0f2bf48b4b0b6ce86300f809853f7886773
MattiaRepetti <mattia.repetti@mail.polimi.it>
2023-03-20 09:07:58 +0100

    Implemented class LobbyTest

--- SW-Eng-Project/src/test/java/GC_11/LobbyTest.java
+++ SW-Eng-Project/src/test/java/GC_11/LobbyTest.java
@@ -1,6 +1,46 @@
 package GC_11;
 
+import GC_11.model.Lobby;
+import GC_11.model.Player;
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.*;
+
 public class LobbyTest {
     //TODO: To be implemented by Mattia
+
+    @Test
+    public void testHasPlayer(){
+        Lobby lobby = new Lobby(2);
+        Player p1 = new Player("Name1");
+        Player p2 = new Player("Name2");
+        Player p3 = new Player("Name3");
+        lobby.addPlayer(p1);
+        lobby.addPlayer(p2);
+
+        assertEquals(lobby.getPlayers().contains(p1),lobby.hasPlayer(p1));
+        assertEquals(lobby.getPlayers().contains(p3),lobby.hasPlayer(p3));
+
+    }
+
+    @Test
+    public void testNumOfPlayers(){
+        Lobby lobby = new Lobby(2);
+        Player p1 = new Player("Name1");
+        Player p2 = new Player("Name2");
+        Player p3 = new Player("Name3");
+        lobby.addPlayer(p1);
+        lobby.addPlayer(p2);
+        assertEquals(lobby.getPlayers().size(), lobby.getMaxPlayers());
+    }
+
+    @Test
+    public void noDuplicateInPlayerList(){
+        Lobby lobby = new Lobby(2);
+        Player p1 = new Player("Name1");
+        lobby.addPlayer(p1);
+        lobby.addPlayer(p1);
+        assertEquals(1,lobby.getPlayers().size());
+    }
 
 }
