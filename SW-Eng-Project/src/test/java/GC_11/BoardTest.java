d4eefce595facf67272f656ef667bca20817b54b
MattiaRepetti <mattia.repetti@mail.polimi.it>
2023-03-21 11:45:39 +0100

    Write backbone of class BoardTest

--- SW-Eng-Project/src/test/java/GC_11/BoardTest.java
+++ SW-Eng-Project/src/test/java/GC_11/BoardTest.java
@@ -1,9 +1,29 @@
 package GC_11;
 
+import org.junit.jupiter.api.Test;
+
 public class BoardTest {
     //TODO: To be implemented by Mattia
 
 
+    @Test
+    void checkRightInit() {
+
+        // Check if the board is correctly initialized according to the number of players.
+
+    }
+
+    @Test
+    void checkTiledRemoved(){
+
+        // Check if a tile is actually removed after being drawn by a player
+    }
+
+    @Test
+    void checkRefill(){
+
+        // Check if the board is correctly refilled
+    }
 
 
 }
