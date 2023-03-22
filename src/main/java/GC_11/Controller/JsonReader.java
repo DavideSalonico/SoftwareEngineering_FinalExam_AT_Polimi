612d185d64821d453b9b69aeac33378164a3f13f
Jaskaran <jaskaran.ram@mail.polimi.it>
2023-03-22 22:56:14 +0100

    SetGoals() method

--- src/main/java/GC_11/Controller/JsonReader.java
+++ src/main/java/GC_11/Controller/JsonReader.java
@@ -1,19 +1,36 @@
 package GC_11.Controller;
 
-import GC_11.model.Triplet;
-import java.util.List;
+import GC_11.model.PersonalGoalCard;
 import GC_11.model.Player;
+import GC_11.model.Triplet;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
 
 public class JsonReader {
     private List <Triplet> read;
     private List <Player> players;
+    private JSONParser parser;
 
     public JsonReader(List<Player> players) {
         this.players = players;
+        JSONParser parser = new JSONParser();
     }
 
-    public void setGoals(){
+    public void setGoals() throws FileNotFoundException {
+        Object obj = parser.parse(new FileReader("path of JSON FILE"));
+        JSONObject jsonObject =  (JSONObject) obj;
 
+        for( Player p : players) {
+            int id = (int) jsonObject.get("id");
+            PersonalGoalCard card;
+
+        }
+
+        // https://stackoverflow.com/questions/10926353/how-to-read-json-file-into-java-with-simple-json-library
     }
 
 
