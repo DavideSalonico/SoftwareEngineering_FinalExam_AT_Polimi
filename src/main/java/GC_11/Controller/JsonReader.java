e945aa7c7d360f64908df6fc04ed7bdb906ec2be
Jaskaran <jaskaran.ram@mail.polimi.it>
2023-03-22 22:39:04 +0100

    Update JsonReader

--- src/main/java/GC_11/Controller/JsonReader.java
+++ src/main/java/GC_11/Controller/JsonReader.java
@@ -1,2 +1,21 @@
-package GC_11.Controller;public class JsonReader {
+package GC_11.Controller;
+
+import GC_11.model.Triplet;
+import java.util.List;
+import GC_11.model.Player;
+
+public class JsonReader {
+    private List <Triplet> read;
+    private List <Player> players;
+
+    public JsonReader(List<Player> players) {
+        this.players = players;
+    }
+
+    public void setGoals(){
+
+    }
+
+
+
 }
