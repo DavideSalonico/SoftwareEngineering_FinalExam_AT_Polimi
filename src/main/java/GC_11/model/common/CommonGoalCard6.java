0dd61bf3d39cb043b04efc39c4ac9f58eb4d21fa
LorenzoReitani <lorenzo.reitani@mail.polimi.it>
2023-03-25 19:16:42 +0100

    Created the new common goal card 6

---
+++ src/main/java/GC_11/model/common/CommonGoalCard6.java
@@ -0,0 +1,39 @@
+package GC_11.model.common;
+
+import GC_11.exceptions.columnIndexOutOfBoundsException;
+import GC_11.model.Player;
+import GC_11.model.TileColor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CommonGoalCard6 extends CommonGoalCard{
+
+    private final String text = "Two columns each formed by 6 different types of tiles.";
+
+    /**
+     * This method check if the common goal of the card has been achieved and in this case adds points to the player
+     * @param player is the player to which you want to control the shelf
+     * @throws columnIndexOutOfBoundsException when trying to control a position outside the matrix
+     */
+    @Override
+    public void check(Player player) throws columnIndexOutOfBoundsException {
+
+        int correctColumns=0;
+        for (int c=0; c<5; c++){
+            Set<TileColor> colors = new HashSet<TileColor>();
+            for (int l=0; l<6; l++){
+                if(!player.getShelf().getTile(l,c).getColor().equals(TileColor.EMPTY)){
+                    colors.add(player.getShelf().getTile(l,c).getColor());
+                }
+            }
+            if (colors.size()==6){
+                correctColumns++;
+            }
+        }
+        if (correctColumns >= 2){
+            givePoints(player);
+        }
+
+    }
+}
