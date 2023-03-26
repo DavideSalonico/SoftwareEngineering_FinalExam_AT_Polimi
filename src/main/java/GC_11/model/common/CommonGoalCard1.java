9d40d7e9529b7bacfb6e6b2fde2623023a1c50a7
LorenzoReitani <lorenzo.reitani@mail.polimi.it>
2023-03-26 23:09:55 +0200

    added text in common goal card 1

--- src/main/java/GC_11/model/common/CommonGoalCard1.java
+++ src/main/java/GC_11/model/common/CommonGoalCard1.java
@@ -4,10 +4,12 @@ import GC_11.model.Player;
 
 public class CommonGoalCard1 extends CommonGoalCard{
 
-
+    private final String text = "Four groups each containing at least" +
+            "4 tiles of the same type (not necessarily in the depicted shape)." +
+            "The tiles of one group can be different from those of another group.";
 
     @Override
-    public void check(Player player) {
+    public void check(Player player)  {
 
     }
 }
