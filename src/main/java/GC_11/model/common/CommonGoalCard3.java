09089426d268fecea8ff2936e5a4af770d5474ea
LorenzoReitani <lorenzo.reitani@mail.polimi.it>
2023-03-28 12:14:27 +0200

    worked on common goal card 3 and added a test

--- src/main/java/GC_11/model/common/CommonGoalCard3.java
+++ src/main/java/GC_11/model/common/CommonGoalCard3.java
@@ -2,6 +2,8 @@ package GC_11.model.common;
 
 import GC_11.exceptions.columnIndexOutOfBoundsException;
 import GC_11.model.Player;
+import GC_11.model.TileColor;
+
 
 public class CommonGoalCard3 extends CommonGoalCard{
 
@@ -9,9 +11,61 @@ public class CommonGoalCard3 extends CommonGoalCard{
             "4 tiles of the same type (not necessarily in the depicted shape)." +
             "The tiles of one group can be different from those of another group.";
 
+    boolean[][] controlMatrix = new boolean[6][5];
 
+
+    private boolean getControlMatrix(int l, int c){
+
+        return controlMatrix [l][c];
+
+    }
+
+    private void setControlMatrixTrue(int l, int c){
+
+        controlMatrix [l][c]= true;
+
+    }
+
+    private void resetControlMatrix(){
+
+        controlMatrix = new boolean[6][5];
+
+    }
     @Override
     public void check(Player player) throws columnIndexOutOfBoundsException {
 
+        resetControlMatrix();
+        int counterTiles = 0;
+        int counterGroups=0;
+        for(int l = 0; l<6; l++){
+            for(int c=0; c<5; c++) {
+                if(!getControlMatrix(l,c)) {
+                    setControlMatrixTrue(l,c);
+                    counterTiles = verify(player, l, c+1, player.getShelf().getTile(l, c).getColor())+
+                            verify(player,l+1,c,player.getShelf().getTile(l,c).getColor());
+                    if(counterTiles>=4){
+                        counterGroups++;
+                    }
+                }
+            }
+        }
+        if(counterGroups>=4){
+            givePoints(player);
+        }
+    }
+
+    public int verify (Player player, int l, int c, TileColor color) throws columnIndexOutOfBoundsException {
+
+        if(l>5 || c>4){
+            return 0;
+        }
+        else if(!getControlMatrix(l,c)){
+            if(player.getShelf().getTile(l,c).getColor()==color){
+                setControlMatrixTrue(l,c);
+                return  1 + verify(player, l, c+1, player.getShelf().getTile(l, c).getColor())+
+                        verify(player,l+1,c,player.getShelf().getTile(l,c).getColor());
+            }else {return 0;}
+        }
+        else {return 0;}
     }
 }
---
+++ src/test/java/GC_11/model/common/CommonGoalCard1Test.java
@@ -0,0 +1,21 @@
+package GC_11.model.common;
+
+import GC_11.model.Player;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommonGoalCard1Test {
+
+    @BeforeAll
+    Player player= new Player("nome", );
+
+    @Test
+    void checkTest() {
+    }
+
+    @Test
+    void verifyTest() {
+    }
+}
