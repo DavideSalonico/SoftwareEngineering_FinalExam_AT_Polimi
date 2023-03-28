96882b51530c57d7433d30bd47d3e8030f6aab62
Jaskaran <jaskaran.ram@mail.polimi.it>
2023-03-28 14:42:15 +0200

    CLIview

--- src/main/java/GC_11/View/CLIview.java
+++ src/main/java/GC_11/View/CLIview.java
@@ -1,2 +1,11 @@
-package GC_11.View;public class CLIview {
+package GC_11.View;
+
+import java.io.Serializable;
+
+public class CLIview implements Serializable {
+    private final Choice controllerChoice;
+    private final Choice playerChoice;
+    private final Outcome outcome;
+
+    // Capire come vogliamo costruire le classi di Choice e di Outcome (output)
 }
