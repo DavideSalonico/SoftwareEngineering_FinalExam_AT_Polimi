ff7ad529c6de2e38c47c446dd08f75c154409333
LorenzoReitani <lorenzo.reitani@mail.polimi.it>
2023-03-30 16:06:35 +0200

    modified

--- src/main/java/GC_11/model/common/CommonGoalCard3.java
+++ src/main/java/GC_11/model/common/CommonGoalCard3.java
@@ -41,7 +41,7 @@ public class CommonGoalCard3 extends CommonGoalCard{
             for(int c=0; c<5; c++) {
                 if(!getControlMatrix(l,c)) {
                     setControlMatrixTrue(l,c);
-                    counterTiles = verify(player, l, c+1, player.getShelf().getTile(l, c).getColor())+
+                    counterTiles = 1+ verify(player, l, c+1, player.getShelf().getTile(l, c).getColor())+
                             verify(player,l+1,c,player.getShelf().getTile(l,c).getColor());
                     if(counterTiles>=4){
                         counterGroups++;
