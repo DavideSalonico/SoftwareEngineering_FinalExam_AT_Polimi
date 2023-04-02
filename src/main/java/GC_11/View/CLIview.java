559be8296d7627f7d892f0114a1075f92e9e51d7
MattiaRepetti <mattia.repetti@mail.polimi.it>
2023-04-02 20:22:15 +0200

    Added class turn

--- src/main/java/GC_11/View/CLIview.java
+++ src/main/java/GC_11/View/CLIview.java
@@ -1,16 +1,15 @@
-/*
+
 package GC_11.View;
 
 import java.io.Serializable;
 
 public class CLIview implements Serializable /*implements Observer*/ {
 
-    /*
     private final Choice controllerChoice;
     private final Choice playerChoice;
     private final Outcome outcome;
-    */
+
 
     // Capire come vogliamo costruire le classi di Choice e di Outcome (output)
 }
-*/
+
