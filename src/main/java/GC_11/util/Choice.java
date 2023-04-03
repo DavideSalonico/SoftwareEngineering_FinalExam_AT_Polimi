444805c8644f18bd63356c252e4ff3e6d915946d
Jaskaran <jaskaran.ram@mail.polimi.it>
2023-04-03 17:14:01 +0200

    CLass Choice

---
+++ src/main/java/GC_11/util/Choice.java
@@ -0,0 +1,15 @@
+package GC_11.util;
+
+public enum Choice {
+    INSERT_NAME,
+    LOGIN,
+    FIND_MATCH,
+    SEE_COMMONGOAL,
+    SEE_PERSONALGOAL,
+    SELECT_TILE,
+    PICK_COLUMN,
+    CHOOSE_ORDER;
+
+
+
+}
