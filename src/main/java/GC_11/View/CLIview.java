4aa1a93bdcb2a4cb9496f911a9dc57b4e2dc5661
MattiaRepetti <mattia.repetti@mail.polimi.it>
2023-04-03 17:56:55 +0200

    Implemented part of CLI view

--- src/main/java/GC_11/View/CLIview.java
+++ src/main/java/GC_11/View/CLIview.java
@@ -1,14 +1,25 @@
 
 package GC_11.View;
 
+import GC_11.util.Observable;
+import GC_11.util.Observer;
+import GC_11.util.Choice;
+
+import java.awt.*;
 import java.io.Serializable;
 
-public class CLIview implements Serializable /*implements Observer*/ {
+public class CLIview /*implements Serializable*/ extends Observable<Choice> implements Runnable  {
 
     // private final Choice controllerChoice;
     // private final Choice playerChoice;
     // private final Outcome outcome;
 
+    @Override
+    public void run(){
+
+        // Lancia la visuale e chiede ai player cosa vogliono fare
+
+    }
 
     // Capire come vogliamo costruire le classi di Choice e di Outcome (output)
 }
