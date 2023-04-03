c1585e5cfb31700ac02612b7d66647d5e75a272c
MattiaRepetti <mattia.repetti@mail.polimi.it>
2023-04-03 18:03:47 +0200

    Implemented another method of the view

--- src/main/java/GC_11/View/CLIview.java
+++ src/main/java/GC_11/View/CLIview.java
@@ -7,6 +7,9 @@ import GC_11.util.Choice;
 
 import java.awt.*;
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class CLIview /*implements Serializable*/ extends Observable<Choice> implements Runnable  {
 
@@ -16,11 +19,40 @@ public class CLIview /*implements Serializable*/ extends Observable<Choice> impl
 
     @Override
     public void run(){
+        while(true){
+
+            // Controllare tramite view che venga eseguita solo quando è il turno del giocatore giusto
+
+            Choice choice = getPlayerChoice();
+            setChanged();
+            notifyObservers();
+        }
+
 
         // Lancia la visuale e chiede ai player cosa vogliono fare
+
 
     }
 
     // Capire come vogliamo costruire le classi di Choice e di Outcome (output)
+
+    public Choice getPlayerChoice(){
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Options available: ");
+        System.out.println( "Signs: " +
+                Arrays.stream(Choice.values())
+                        .map(Choice::name)
+                        .collect(
+                                Collectors.joining(",", "[", "]")));
+        while (true){
+            String input = s.next();
+            try {
+                return Choice.valueOf(input);
+            } catch(IllegalArgumentException e){
+                System.err.println("Invalid choice. Please retake.");
+            }
+        }
+    }
 }
 
