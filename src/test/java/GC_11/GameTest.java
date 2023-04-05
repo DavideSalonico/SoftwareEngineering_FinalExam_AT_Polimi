a4defacce83ded9181f4d550db45f18a5a5b6bec
Davide Salonico <davide.salonico@gmail.com>
2023-04-05 07:19:31 +0200

    added game test cases

--- src/test/java/GC_11/GameTest.java
+++ src/test/java/GC_11/GameTest.java
@@ -1,6 +1,57 @@
 package GC_11;
 
-public class GameTest {
-    //TODO: To be implemented by Rei
+import GC_11.model.Game;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+public class GameTest {
+    private Game game;
+    private List<String> players = new ArrayList<String>();
+    @BeforeEach
+    public void createGame(){
+        String name1 = "Lorenzo";
+        String name2 = "Davide";
+        String name3 = "Mattia";
+        String name4 = "Jaskaran";
+
+        players.add(name1);
+        players.add(name2);
+        players.add(name3);
+        players.add(name4);
+
+        game = new Game(players);
+    }
+
+    @DisplayName("Right players order")
+    @Test
+    public void rightOrder(){
+        assertEquals(players.size(), game.getPlayers().size());
+
+        for(int i=0; i < players.size(); i++){
+            assertEquals(players.get(i), game.getPlayers().get(i).getNickname());
+        }
+    }
+
+    @DisplayName("Right current player")
+    @Test
+    public void rightCurrentPlayer(){
+        assertEquals(game.getCurrentPlayer(), game.getPlayers().get(0));
+        //TODO: expand when we add turn logic to controller
+    }
+
+    @DisplayName("No null pointers")
+    @Test
+    public void noNull(){
+        assertNotNull(game.getBoard());
+        assertNotNull(game.getCommonGoal(0));
+        assertNotNull(game.getCommonGoal(1));
+    }
 }
