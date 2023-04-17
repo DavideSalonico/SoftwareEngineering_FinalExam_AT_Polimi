21818e94efffc9bae69b6827bb41273d46399f5e
MattiaRepetti <mattia.repetti@mail.polimi.it>
2023-04-17 14:10:00 +0200

    Finished server stub

--- src/main/java/GC_11/distributed/socket/ServerStub.java
+++ src/main/java/GC_11/distributed/socket/ServerStub.java
@@ -1,6 +1,8 @@
 package GC_11.distributed.socket;
 
 import GC_11.distributed.Client;
+import GC_11.model.GameView;
+import GC_11.util.Choice;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -22,13 +24,68 @@ public class ServerStub {
         this.port=port;
     }
 
+
     public void register(Client client) throws RemoteException{
         try
         {
             this.socket=new Socket(ip,port);
+            try{
+                this.oos = new ObjectOutputStream(socket.getOutputStream());
+            }catch(IOException e){
+                throw new RemoteException("Cannot create output stream", e);
+            }
+            try {
+                this.ois = new ObjectInputStream(socket.getInputStream());
+            } catch (IOException e) {
+                throw new RemoteException("Cannot create input stream", e);
+            }
         }
         catch(IOException e){
-
+            throw new RemoteException("Unable to connect to the server", e);
         }
     }
+
+    public void update(Client client, Choice arg) throws RemoteException {
+        try{
+            oos.writeObject(arg);
+        }catch(IOException e){
+            throw new RemoteException("Cannot receive model view from client");
+        }
+    }
+
+    public void receive(Client client) throws RemoteException{
+        GameView gm;
+        try{
+            gm = (GameView) ois.readObject();
+        }
+        catch (IOException e){
+            throw new RemoteException("Cannot receive model view from client", e);
+        }
+        catch(ClassNotFoundException e)
+        {
+            throw new RemoteException("Cannot deserialize model view from client",e);
+        }
+        /*
+        Turn.Event arg;
+        try {
+            arg = (Turn.Event) ois.readObject();
+        } catch (IOException e) {
+            throw new RemoteException("Cannot receive event from client", e);
+        } catch (ClassNotFoundException e) {
+            throw new RemoteException("Cannot deserialize event from client", e);
+        }
+        */
+
+        // client.update(gm, arg)
+    }
+
+    public void close() throws RemoteException {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            throw new RemoteException("Cannot close socket", e);
+        }
+    }
+
+
 }
