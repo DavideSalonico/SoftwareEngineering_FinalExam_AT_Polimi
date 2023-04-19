df3425a7ce22b4ee673cd08075db8c43d8790328
Davide Salonico <davide.salonico@gmail.com>
2023-04-19 12:24:22 +0200

    added ChoiceTest

--- src/test/java/GC_11/util/ChoiceTest.java
+++ src/test/java/GC_11/util/ChoiceTest.java
@@ -1,2 +1,20 @@
-package GC_11.util;public class ChoiceTest {
+package GC_11.util;
+
+import GC_11.model.Player;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.*;
+
+public class ChoiceTest {
+
+    @DisplayName("IllegalArgumentException")
+    @Test
+    public void throwsIllegalArgumentException(){
+        Player player = new Player("TestPlayer");
+        try{
+            Choice choice = new Choice(player, "CIAO 7");
+            fail("Choice builder didn't throw the InvalidArgumentException when supposed to");
+        } catch (IllegalArgumentException e){}
+    }
 }
