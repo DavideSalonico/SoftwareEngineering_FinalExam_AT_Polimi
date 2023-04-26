506d633f1fb813ffd014fed15038e3897ad35f81
MattiaRepetti <mattia.repetti@mail.polimi.it>
2023-04-26 11:51:15 +0200

    Implemented network

--- src/main/java/GC_11/distributed/socket/ClientGame.java
+++ src/main/java/GC_11/distributed/socket/ClientGame.java
@@ -1,5 +1,6 @@
 package GC_11.distributed.socket;
 
+import GC_11.model.GameView;
 import GC_11.model.Player;
 import GC_11.util.Choice;
 import GC_11.view.View;
@@ -9,6 +10,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.rmi.RemoteException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -19,6 +21,8 @@ public class ClientGame {
     private int port;
     private Player player;
 
+    private boolean active = true;
+
     private ObjectInputStream inputStream;
     private ObjectOutputStream outputStream;
 
@@ -28,8 +32,18 @@ public class ClientGame {
         this.player=new Player();
     }
 
+    public synchronized boolean isActive(){
+        return this.active;
+    }
+
+    public synchronized void setActive(boolean active){
+        this.active=active;
+    }
+
+
     public void startClient() throws IOException, ClassNotFoundException {
         Socket socket = new Socket(ip,port);
+        System.out.println("---Client---");
         System.out.println("Connection established");
         System.out.println("Initializing output stream...");
         try {
@@ -49,22 +63,45 @@ public class ClientGame {
             System.err.println("Cannot get input stream");
             System.err.println(e.getMessage());
         }
-        //
-
-        System.out.println("Sending choice:");
-        Choice choice = new Choice(player,"INSERT_NAME MATTIA");
-        outputStream.writeObject(choice);
-        outputStream.flush();
-
-        String response = (String) this.inputStream.readObject();
-        System.out.println("Received " + response);
-
-
+        Scanner commandInput = new Scanner(System.in);
+        while(true){
+            String command = commandInput.nextLine();
+            try{
+                if (command.equals("quit")){
+                    break;
+                }
+                else{
+                    Choice choiceToSend = new Choice(player,command);
+                    System.out.println("Sending choice...");
+                    outputStream.writeObject(choiceToSend);
+                    outputStream.flush();
+                    String response = (String) this.inputStream.readObject();
+                    System.out.println("Received " + response);
+                }
+            }catch (IllegalArgumentException e){
+                System.out.println("Errore nel comando");
+            }
+        }
 
         outputStream.close();
         inputStream.close();
         socket.close();
     }
+
+    public void sendChoice(Choice choice) throws RemoteException{
+        try{
+            outputStream.writeObject(choice);
+        }
+        catch(IOException e){
+            throw new RemoteException("Cannot send choice to the server",e);
+        }
+    }
+
+
+
+
+
+
 
 }
 
--- src/main/java/GC_11/distributed/socket/ServerGame.java
+++ src/main/java/GC_11/distributed/socket/ServerGame.java
@@ -1,5 +1,6 @@
 package GC_11.distributed.socket;
 
+import GC_11.distributed.Client;
 import GC_11.util.Choice;
 
 import java.io.IOException;
@@ -8,22 +9,33 @@ import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.rmi.RemoteException;
 import java.util.Scanner;
 
 public class ServerGame {
     private int port;
     private ServerSocket serverSocket;
+
+
     private ObjectOutputStream outputStream;
     private ObjectInputStream inputStream;
 
     public ServerGame(int port){
+
+        // Opening TCP port
         this.port=port;
+        try{
+            this.serverSocket = new ServerSocket(this.port);
+            System.out.println("---Server---");
+            System.out.println("Server ready on port: "+ port);
+        }
+        catch(IOException e){
+            System.out.println("Error during initialization phase...");
+            System.out.println(e.getMessage());
+        }
     }
 
     public void startServer() throws IOException, ClassNotFoundException {
-        // Opening TCP port
-        serverSocket = new ServerSocket(port);
-        System.out.println("Server socket ready on port: "+port);
 
         // Waiting for connection
         Socket socket = serverSocket.accept();
@@ -64,17 +76,36 @@ public class ServerGame {
         System.out.println("Closing socket");
         in.close();
         out.close();*/
-        Choice choice = (Choice) inputStream.readObject();
-        System.out.println("Received: " + choice.getChoice() + choice.getParams());
 
-        String response = "Risposta a: " + choice.getChoice() + choice.getParams();
-        outputStream.writeObject(response);
-        outputStream.flush();
+        while(true){
+            Choice choice = (Choice) inputStream.readObject();
+            System.out.println("Received: " + choice.getChoice() + choice.getParams() + "from " + choice.getPlayer());
 
+            String response = "Risposta a: " + choice.getChoice() + choice.getParams();
+            outputStream.writeObject(response);
+            outputStream.flush();
+            if(choice.getParams().size()>0 && choice.getParams().get(0).equals("quit")){
+                break;
+            }
+        }
         socket.close();
         outputStream.close();
         inputStream.close();
         serverSocket.close();
+        }
+
+        public void receiveChoice(ClientGame client) throws RemoteException {
+            Choice clientChoice;
+            try{
+                clientChoice = (Choice) inputStream.readObject();
+            }
+            catch (IOException e){
+                throw new RemoteException("Cannot receive choice from client", e);
+            }
+            catch (ClassNotFoundException e){
+                throw new RemoteException("Cannot deserialize choice from client", e);
+            }
+
         }
 }
 
