1afda74d9452b9914055b31437acad2f08d1f3f3
MattiaRepetti <mattia.repetti@mail.polimi.it>
2023-04-29 12:13:50 +0200

    Implementing Lobby TUI

---
+++ src/main/java/GC_11/network/AppLobbyTUI.java
@@ -0,0 +1,15 @@
+package GC_11.network;
+
+import GC_11.view.LobbyTUI;
+
+public class AppLobbyTUI {
+
+    public static void main(String[] args){
+        LobbyTUI lt = new LobbyTUI();
+        lt.run();
+
+
+    }
+
+
+}
--- src/main/java/GC_11/view/LobbyTUI.java
+++ src/main/java/GC_11/view/LobbyTUI.java
@@ -1,6 +1,53 @@
 package GC_11.view;
 
-public class LobbyTUI {
+import GC_11.network.LobbyView;
+
+import java.util.Scanner;
+
+public class LobbyTUI implements Runnable{
+
+    private LobbyView lobbyView;
+    Scanner inputLine;
+
+    public LobbyTUI(){
+        this.inputLine = new Scanner(System.in);
+    }
+
+    public void run(){
+        printWelcome();
+
+    }
+
+    public void printWelcome(){
+
+        System.out.println("Benvenuto su MyShelf. Come desideri proseguire?\n1 - Connettiti al server");
+        String userCommand = inputLine.nextLine();
+        evaluateUserCommand(userCommand);
+
+    }
+
+    private void evaluateUserCommand(String command){
+        if(command.equals("1")){
+            System.out.println("Vuoi utilizzare RMI o socket?\n1 - Socket\n2 - RMI [Attualmente non disponibile]\n");
+            String input = inputLine.nextLine();
+            while (input.equals("1") || input.equals("2")){
+                if (input.equals("1")){
+                    System.out.println("Connessione al socket");
+                    // socketConnect();
+                }
+                else if (input.equals("2")){
+                    System.out.println("Connessione a RMI");
+                    //RMIConnect();
+                }
+                else{
+                    System.out.println("Input non riconosciuto. Reinserire");
+                    input=inputLine.nextLine();
+                }
+            }
+        }
+    }
+
+
 
 
 
