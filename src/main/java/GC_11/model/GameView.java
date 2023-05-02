f8cf7edba49ea085d639a76bc797142b9f217e3c
Jaskaran <jaskaran.ram@mail.polimi.it>
2023-05-02 16:30:47 +0200

    GameView

--- src/main/java/GC_11/model/GameView.java
+++ src/main/java/GC_11/model/GameView.java
@@ -15,18 +15,7 @@ public class GameView implements Serializable {
     //The serialization process at runtime associates an id with each Serializable class which is known as SerialVersionUID.
     //The sender and receiver must have the same SerialVersionUID, otherwise, InvalidClassException will be thrown when you deserialize the object.
     private static final long serialVersionUID = 2L;
-
-    /** NON so ancora se servono
-    public String playerNickname;
-    public String PersonalGoalText;
-    public String[] CommonGoalText;
-
-    //From the first player in order
-    public int[] playersPoints;
-     */
-
     private boolean error;
-
     private String exceptionMessage;
     private final Game model;
 
@@ -37,8 +26,12 @@ public class GameView implements Serializable {
         if(exception != null){
             this.error = true;
             this.exceptionMessage = exception.getMessage();
+            this.model = null;
+        }else {
+            this.model = model;
+            this.error = false;
+            this.exceptionMessage  = null;
         }
-        this.model = model;
     }
 
     public Board getBoard() { return model.getBoard(); }
@@ -51,12 +44,22 @@ public class GameView implements Serializable {
         return model.getCommonGoal();
     }
 
+    /**
+     * This method needs to be called in ServerClientHandler only on a copy of the original view,
+     * otherwise it will set Null the remaining PersonalCards at first call, so it become useless for the others players
+     * @param player
+     */
     public void setPersonalNull(Player player){
         for(Player p : this.getPlayers()){
             if(!p.equals(player)) p.setPersonalGoal(null);
         }
     }
 
+    /**
+     * CommonGoalCard Getter method
+     * @param index or 0 or 1
+     * @return CommonGoalCard at position 'index'
+     */
     public CommonGoalCard getCommonGoalCard(int index) {
         return this.model.getCommonGoal(index);
     }
@@ -65,15 +68,27 @@ public class GameView implements Serializable {
         return this.error;
     }
 
+    /**
+     * Exception's message setter from object Exception
+     * @param e generic exception caught
+     */
     public void setExceptionMessage(Exception e){
         this.exceptionMessage = e.getMessage();
     }
 
+    /**
+     * Exception's message setter directly from String
+     * @param mess Error message string
+     */
     public void setExceptionMessage(String mess){
         this.exceptionMessage = mess;
     }
 
 
+    /**
+     * Exception message getter
+     * @return error message string
+     */
     public String getExceptionMessage(){
         return this.exceptionMessage;
     }
