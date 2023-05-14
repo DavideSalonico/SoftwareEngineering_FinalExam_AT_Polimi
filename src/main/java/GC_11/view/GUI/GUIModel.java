3cdedd13798e70171f24a617a2faf8a09eea8e4e
JaskaranRam <jaskaran.ram@mail.polimi.it>
2023-05-15 00:25:17 +0200

    boh

--- src/main/java/GC_11/view/GUI/GUIModel.java
+++ src/main/java/GC_11/view/GUI/GUIModel.java
@@ -4,6 +4,7 @@ import GC_11.model.GameView;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -19,11 +20,12 @@ public class GUIModel  {
     }
     static GameView model;
     private static String clientNickName;
+    private final MainFrame mainFrame;
 
 
     private static class MainFrame extends JFrame{
 
-        private static final BufferedImage[] background = new BufferedImage[10];
+        private static final BufferedImage[] background = new BufferedImage[8];
         private static final Map<String, BufferedImage> otherPlayers = new HashMap<>();
 
         private static final Map<Integer, BufferedImage> blueTiles  = new HashMap<>();
@@ -35,19 +37,20 @@ public class GUIModel  {
 
         //Empty and Prohibited tiles are managed in different ways
 
-        private static final BufferedImage[] GUIplayer = new BufferedImage[10];
         static {
             try{
-                background[0] = ImageIO.read(new File("src/resources/GraphicalResources/misc/sfondo parquet.png"));
-                background[1] = ImageIO.read(new File("src/resources/GraphicalResources/misc/livingroom.png"));
-                background[2] = ImageIO.read(new File("src/resources/GraphicalResources/misc/bookshelf.png"));
+                background[0] = ImageIO.read(new File("src/resources/GraphicalResources/misc/sfondo parquet.jpg"));
+                background[1] = ImageIO.read(new File("src/resources/GraphicalResources/boards/livingroom.png"));
+                background[2] = ImageIO.read(new File("src/resources/GraphicalResources/boards/bookshelf.png"));
                 background[3] = ImageIO.read(new File("src/resources/GraphicalResources/Publisher material/Title 2000x2000px.png"));
-                background[4] = ImageIO.read(new File("src/resources/GraphicalResources/common goals cards/" + model.getCommonGoalCard(0).id + ".png"));
-                background[5] = ImageIO.read(new File("src/resources/GraphicalResources/common goals cards/" + model.getCommonGoalCard(1).id + ".png"));
-                background[6] = ImageIO.read(new File("src/resources/GraphicalResources/common goals cards/Sacchetto Chiuso.png"));
+                //background[4] = ImageIO.read(new File("src/resources/GraphicalResources/common goals cards/" + model.getCommonGoalCard(0).getId() + ".jpg"));
+                //background[5] = ImageIO.read(new File("src/resources/GraphicalResources/common goals cards/" + model.getCommonGoalCard(1).getId() + ".jpg"));
+                background[6] = ImageIO.read(new File("src/resources/GraphicalResources/misc/Sacchetto Chiuso.png"));
 
                 //ATTENZIONE : getPlayer(nickname) può tornare null
-                background[7] = ImageIO.read(new File("src/resources/GraphicalResources/personal goal card/Personal_Goals"+model.getPlayer(clientNickName).getPersonalGoal().getId()+".png"));
+                //background[7] = ImageIO.read(new File("src/resources/GraphicalResources/personal goal card/Personal_Goals"+model.getPlayer(clientNickName).getPersonalGoal().getId()+".jpg"));
+
+
 
                 for(int i = 1; i<=3; i++){
                     blueTiles.put(i, ImageIO.read(new File("src/resources/GraphicalResources/item tiles/Cornici1."+ i +".png")));
@@ -67,12 +70,68 @@ public class GUIModel  {
                 throw new RuntimeException(e);
             }
         }
+
+        private final JLabel backgroundLabel = new JLabel(new ImageIcon(background[0]));
+        private final JLabel titleLabel= new JLabel();
+        {
+            Image scaledTitle = background[3].getScaledInstance(background[3].getWidth()/2,background[3].getHeight()/2, Image.SCALE_SMOOTH);
+            ImageIcon x = new ImageIcon(scaledTitle);
+            titleLabel.setIcon(x);
+            //titleLabel = new JLabel(new ImageIcon(scaledTitle));
+            setBackground(Color.GRAY);
+        }
+        private final JLabel mainBookShelfLabel = new JLabel();
+        {
+            Image scaledBookShelf = background[2].getScaledInstance(background[2].getWidth()/2,background[2].getHeight()/2, Image.SCALE_SMOOTH);
+            ImageIcon x = new ImageIcon(scaledBookShelf);
+            mainBookShelfLabel.setIcon(x);
+            //mainBookShelfLabel = new JLabel(new ImageIcon(scaledBookShelf));
+            setBackground(Color.GRAY);
+        }
+        private final JLabel boardLabel = new JLabel(new ImageIcon(background[1]));
+
+
+        //private final JLabel commonGoalCard1Label = new JLabel(new ImageIcon(background[4]));
+        //private final JLabel commonGoalCard2Label = new JLabel(new ImageIcon(background[5]));
+        private final JLabel bagLabel = new JLabel(new ImageIcon(background[6]));
+        //private final JLabel personalGoalCardLabel = new JLabel(new ImageIcon(background[7]));
+
+
+        public MainFrame(){
+            super("MY SHELFIE");
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            setSize(1920, 1080);
+            //setResizable(false);
+            JPanel mainPanel = new JPanel(new GridLayout(3,1));
+            JPanel firstRow = new JPanel(new GridLayout(1,2));
+            JPanel secondRow = new JPanel(new GridLayout(1,3));
+            firstRow.add(boardLabel);
+            firstRow.add(mainBookShelfLabel);
+
+            /*
+            for(otherPlayers : otherPlayers.values()){
+                secondRow.add(otherPlayers);
+            }
+            */
+
+            Image scaledTitle = background[3].getScaledInstance(background[3].getWidth(),background[3].getHeight(), Image.SCALE_SMOOTH);
+            Image scaledMainBookShelf = background[2].getScaledInstance(background[3].getWidth(),background[3].getHeight(), Image.SCALE_SMOOTH);
+            mainPanel.add(titleLabel);
+            mainPanel.add(firstRow);
+            //secondRow.setBackground(Color.BLACK);  i colori vengono mostrati solo se non coperti da immagini
+            mainPanel.add(secondRow);
+            add(mainPanel);
+            pack();
+            setVisible(true);
+
+        }
     }
 
     public GUIModel(GameView model, String clientNickName) {
 
         this.model = model;
-        this.clientNickName = clientNickName();
+        this.clientNickName = clientNickName;
+        this.mainFrame = new MainFrame();
     }
 
     public String getClientNickName() {
