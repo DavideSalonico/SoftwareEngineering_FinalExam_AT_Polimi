5989d2c5731d823a4ab599be049e0af6fef443fb
LorenzoReitani <lorenzo.reitani@mail.polimi.it>
2023-06-28 11:27:22 +0200

    added documentation to Triplet

--- src/main/java/GC_11/model/Triplet.java
+++ src/main/java/GC_11/model/Triplet.java
@@ -3,7 +3,8 @@ package GC_11.model;
 import java.io.Serializable;
 
 /**
- * Triplet is a side class which give the possibility to save a combination of row,column and color of a Tile
+ * Triplet is a helper class that represents a combination of row, column, and color of a Tile.
+ * It is used to store and retrieve information about a specific position and color on the board.
  */
 public class Triplet implements Serializable {
 
@@ -11,20 +12,42 @@ public class Triplet implements Serializable {
     private int col;
     private TileColor color;
 
-    public Triplet(int r, int c, TileColor color) {
-        this.row = r;
-        this.col = c;
+    /**
+     * Constructs a Triplet object with the specified row, column, and color.
+     *
+     * @param row   the row of the position
+     * @param col   the column of the position
+     * @param color the color of the Tile
+     */
+    public Triplet(int row, int col, TileColor color) {
+        this.row = row;
+        this.col = col;
         this.color = color;
     }
 
+    /**
+     * Returns the column of the position represented by this Triplet.
+     *
+     * @return the column value
+     */
     public int getCol() {
         return col;
     }
 
+    /**
+     * Returns the row of the position represented by this Triplet.
+     *
+     * @return the row value
+     */
     public int getRow() {
         return row;
     }
 
+    /**
+     * Returns the color of the Tile represented by this Triplet.
+     *
+     * @return the TileColor value
+     */
     public TileColor getColor() {
         return color;
     }
