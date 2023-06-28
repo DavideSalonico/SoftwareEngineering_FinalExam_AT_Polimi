43937f136fcebb173b88bafea843b15bd4de394e
LorenzoReitani <lorenzo.reitani@mail.polimi.it>
2023-06-28 13:00:17 +0200

    added documentation to AddNicknameChoice

--- src/main/java/GC_11/network/choices/AddNicknameChoice.java
+++ src/main/java/GC_11/network/choices/AddNicknameChoice.java
@@ -9,13 +9,32 @@ import GC_11.model.Player;
 import java.rmi.RemoteException;
 import java.util.List;
 
+/**
+ * AddNicknameChoice is a subclass of Choice representing the choice to add a player's nickname.
+ */
 public class AddNicknameChoice extends Choice{
 
+    /**
+     * Constructs an AddNicknameChoice object.
+     *
+     * @param player the player associated with the choice.
+     * @param params the parameters for the choice.
+     * @param type   the type of the choice.
+     * @throws IllegalMoveException if the move is illegal.
+     */
     public AddNicknameChoice(Player player, List<String> params, ChoiceType type) throws IllegalMoveException {
         super(player, params, type);
     }
 
     // TODO: HANDLE EXCEPTIONS
+    /**
+     * Executes the choice on the server by adding the player's nickname to the lobby.
+     *
+     * @param controller the controller to execute the choice on.
+     * @throws RemoteException                   if a remote error occurs.
+     * @throws ExceededNumberOfPlayersException   if the maximum number of players is exceeded.
+     * @throws NameAlreadyTakenException          if the nickname is already taken.
+     */
     @Override
     public void executeOnServer(Controller controller) throws RemoteException, ExceededNumberOfPlayersException, NameAlreadyTakenException {
         controller.getLobby().addPlayer(params.get(0));
