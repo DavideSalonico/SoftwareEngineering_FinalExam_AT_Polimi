8bd15b939c67cc21f60470281ecc40701f9d72a4
MattiaRepetti <mattia.repetti@mail.polimi.it>
2023-06-30 09:48:57 +0200

    Lobby javaDOC

--- src/main/java/GC_11/model/Lobby.java
+++ src/main/java/GC_11/model/Lobby.java
@@ -16,12 +16,27 @@ public class Lobby {
     private List<String> playersNames = new ArrayList<String>();
     private PropertyChangeListener listener;
 
+    /**
+     * Constructs a Lobby object.
+     */
     public Lobby(){}
 
+
+    /**
+     *  Check if a name is already taken by a player in the lobby.
+      * @param playerName The name to check.
+     * @return True if the name is already taken, false otherwise.
+     */
     public synchronized boolean nameAlreadyTaken(String playerName) {
         return playersNames.contains(playerName);
     }
 
+    /**
+     * Adds a player to the lobby.
+     * @param playerName The name of the player to add.
+     * @throws ExceededNumberOfPlayersException If the lobby is full.
+     * @throws NameAlreadyTakenException If the name is already taken.
+     */
     public synchronized void addPlayer(String playerName) throws ExceededNumberOfPlayersException, NameAlreadyTakenException {
         if (playersNames.size() < maxPlayers && !playersNames.contains(playerName)) {
             playersNames.add(playerName);
@@ -32,6 +47,11 @@ public class Lobby {
         }
     }
 
+    /**
+     * Removes a player from the lobby.
+     * @param playerName The name of the player to remove.
+     * @throws PlayerNotInListException If the player is not in the lobby.
+     */
     public synchronized void removePlayer(String playerName) throws PlayerNotInListException {
         if (playersNames.contains(playerName)) {
             playersNames.remove(playerName);
@@ -40,25 +60,39 @@ public class Lobby {
         }
     }
 
+    /**
+     * Checks if the lobby is full.
+     * @return True if the lobby is full, false otherwise.
+     */
     public synchronized boolean isFull() {
         return (playersNames.size() == maxPlayers);
     }
 
-    private synchronized boolean hasPlayer(String playerName) {
-        return (playersNames.contains(playerName));
-    }
+    /**
+     * Gets the list of players in the lobby.
+     * @return The list of players in the lobby.
+     */
 
     public synchronized List<String> getPlayers() {
         return playersNames;
     }
 
+    /**
+     * Gets the maximum number of players in the lobby.
+     * @return The maximum number of players in the lobby.
+     */
     public int getMaxPlayers() {
         return maxPlayers;
     }
 
+    /**
+     * Sets the maximum number of players in the lobby.
+     * @param maxPlayers The maximum number of players in the lobby.
+     */
     public void setMaxPlayers(int maxPlayers) {
         this.maxPlayers = maxPlayers;
     }
+
 
     public void triggerException(Exception e){
         PropertyChangeEvent evt = new PropertyChangeEvent(
