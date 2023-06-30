83ea14a63bcd0f2f8c18d9784bac0e1d849587fd
DavideSalonico <davide.salonico@mail.polimi.it>
2023-06-30 11:31:02 +0200

    error message

--- src/main/java/GC_11/view/View.java
+++ src/main/java/GC_11/view/View.java
@@ -8,7 +8,6 @@ import GC_11.network.message.LobbyViewMessage;
  * all the necessary data during the Game
  */
 public abstract class View {
-    private boolean errorNickname = false;
     protected String nickname;
     protected GameViewMessage modelView;
     public abstract void show();
@@ -29,12 +28,4 @@ public abstract class View {
     }
 
     public abstract void notifyDisconnection();
-
-    public void setErrorNickname(boolean errorNickname) {
-        this.errorNickname = errorNickname;
-    }
-
-    public boolean getErrorNickname() {
-        return errorNickname;
-    }
 }
